b273dd99be6a23c11fb6161a42e7cc1cb048c152
BikkIlshat <heavyilshat@rambler.ru>
2022-06-29 20:50:28 +0500

    Создал модельку для актеров

--- app/src/main/java/com/bikk/filmlibrary/data/retrofit/ApiService.kt
+++ app/src/main/java/com/bikk/filmlibrary/data/retrofit/ApiService.kt
@@ -1,11 +1,16 @@
 package com.bikk.filmlibrary.data.retrofit
 
 import com.bikk.filmlibrary.models.MoviesModel
+import com.bikk.filmlibrary.models.actors.ActorsModels
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 
 interface ApiService {
     @GET("3/movie/popular?api_key=3fe888e646ae612e98d77c0b7a08da79&language=en-US&page=1")
     suspend fun getPopularMovie(): Response<MoviesModel>
+
+    @GET("3/movie/297761/credits?api_key=95fb6cda812295a096fe775d6db46a71&language=en-US")
+    suspend fun getActorsMovie(@Path("id") Int: Int): Response<ActorsModels>
 }
--- app/src/main/java/com/bikk/filmlibrary/di/modules/remote/RemoteModule.kt
+++ app/src/main/java/com/bikk/filmlibrary/di/modules/remote/RemoteModule.kt
@@ -2,10 +2,14 @@ package com.bikk.filmlibrary.di.modules.remote
 
 import com.bikk.filmlibrary.data.retrofit.ApiService
 import com.bikk.filmlibrary.models.MoviesModel
+import com.bikk.filmlibrary.models.actors.ActorsModels
 import retrofit2.Response
 
 class RemoteModule(private val apiService: ApiService) : RemoteModuleInt {
     override suspend fun getMovies(): Response<MoviesModel> {
         return apiService.getPopularMovie()
+    }
+    override suspend fun getActorsMovie(id: Int): Response<ActorsModels> {
+        return apiService.getActorsMovie(id)
     }
 }
